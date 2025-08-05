82ab05999258a8d86c3dd1a254f121f714b4cb87
Vishwalekha2001 <vishwalekhad@gmail.com>
2025-08-05 11:15:46 +0530

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,4 +1,14 @@
 
+
+
+import math
+
 def calculateStats(numbers):
-  # implement the computation of statistics here and return the results
-  return None
+    if not numbers:
+        return {"avg": math.nan, "max": math.nan, "min": math.nan}
+    
+    avg = sum(numbers) / len(numbers)
+    max_value = max(numbers)
+    min_value = min(numbers)
+    
+    return {"avg": avg, "max": max_value, "min": min_value}
